a9eb7d951c1fb4913192bc005d2151421e3d55d2
Charan-Reddy-Thambireddy <80142686+Charan-Reddy-Thambireddy@users.noreply.github.com>
2022-03-10 13:26:29 +0530

    login package changes

--- ApprovalManagementSystem/src/app/app.module.ts
+++ ApprovalManagementSystem/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { EmiLogModule } from 'emi-log';
+import {  EmiLogModule } from 'emi-log';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Components/home/home.component';
